268da468cb223e0a06bcb8625c4d390fed0a7bdc
Tom Owers <tom.owers@deliveroo.co.uk>
2018-09-21 14:13:38 +0100

    Added the ability to append words instead of a timestamp

--- email-clipboard.rb
+++ email-clipboard.rb
@@ -1,6 +1,11 @@
 require 'clipboard'
+require_relative 'words'
 
 def email_to_copy
+  if ARGV.first == '-word'
+    return "#{mailbox}+#{word}@#{domain}"
+  end
+
   "#{mailbox}+#{Time.now.to_i}@#{domain}"
 end
 
@@ -14,6 +19,10 @@ end
 
 def email
   ENV.fetch('EMAIL_CLIPBOARD_ADDRESS', '')
+end
+
+def word
+  Words.new.words.sample.downcase
 end
 
 Clipboard.copy(email_to_copy)
